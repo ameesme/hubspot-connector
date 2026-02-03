0e2e775c7195d949b5ccce763b4e0cb0137a53b5
Mees <git@amees.me>
2026-02-03 14:53:09 +0100

    Avoiding duplicate reporting in Hubspot since invoices are now always created

--- src/routes/stripe-webhook.ts
+++ src/routes/stripe-webhook.ts
@@ -23,60 +23,6 @@ async function handleStripeWebhook(
 
   console.log(`[WEBHOOK] Received Stripe event "${eventType}"`);
 
-  if (eventType === "checkout.session.completed") {
-    const event = req.body as Stripe.CheckoutSessionCompletedEvent;
-
-    console.log(
-      `[WEBHOOK] Payment completed for session ${event.data.object.id}`,
-    );
-    const customerEmail =
-      event.data.object.customer_email ||
-      event.data.object.customer_details?.email;
-    const totalAmount = event.data.object.amount_total;
-    const currency = event.data.object.currency;
-    const recurring = event.data.object.mode === "subscription";
-
-    // Get Stripe Customer
-    const customer = await stripe.customers.retrieve(
-      event.data.object.customer as string,
-    );
-
-    if (
-      !customerEmail ||
-      !totalAmount ||
-      !currency ||
-      typeof recurring !== "boolean"
-    ) {
-      console.log("[WEBHOOK] Invalid event data", event);
-      res.status(400).send("Invalid event data");
-      return;
-    }
-
-    if (!customer || customer.deleted) {
-      console.log("[WEBHOOK] Customer not found", event);
-      res.status(200).send("Customer not found");
-      return;
-    }
-
-    // Get customer metadata
-    const metadata = customer.metadata;
-
-    // Check if the customer has hubspot-integration
-    if (!metadata || !metadata["hubspot-integration"]) {
-      console.log("[WEBHOOK] Customer not found", event);
-      res.status(200).send("Customer not found");
-      return;
-    }
-
-    await submitStripePaymentReceipt({
-      email: customerEmail,
-      amountInCents: totalAmount,
-      currency,
-      recurring,
-      companyURL: metadata["company-url"],
-    });
-  }
-
   if (eventType === "invoice.paid") {
     const event = req.body as Stripe.InvoicePaidEvent;
 
@@ -87,17 +33,6 @@ async function handleStripeWebhook(
     const totalAmount = event.data.object.amount_paid;
     const currency = event.data.object.currency;
     const recurring = !!event.data.object.subscription;
-
-    if (
-      eventType === "invoice.paid" &&
-      event.data.object.billing_reason === "subscription_create"
-    ) {
-      console.log(
-        "[WEBHOOK] Skipping first subscription invoice (already handled by checkout.session.completed)",
-      );
-      res.status(200).send();
-      return;
-    }
 
     // Get Stripe Customer
     const customer = await stripe.customers.retrieve(
